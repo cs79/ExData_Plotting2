832b5ed565fb606cbf44c788b657744741c8c9df
cs79 <cloudstrife79@gmail.com>
2014-05-20 14:14:42 -0400

    cosmetic fixes to plot3 files

--- plot3.R
+++ plot3.R
@@ -22,14 +22,14 @@ balt <- nei[nei$fips=="24510",])
 sumByYearType <- aggregate(balt$Emissions, 
                            by=list(Year=balt$year, Type=balt$type), FUN=sum)
 
-## open graphics device - dimensions are larger than default for clarity
+## open graphics device - dimensions are different than default for clarity
 png(file = "plot3.png", width = 800, height = 400, units = "px")
 
-## plot with facet on source ?  this doesn't quite work, because i'd rather see 
-## totals by type
+## plot the totals faceted by type
 library(ggplot2)
 qplot(data=sumByYearType, Year, x, facets = .~Type, 
-      main="Baltimore, MD Total Emission Trends by Source Type", xlab="Year")
+      main="Baltimore, MD Total Emission Trends by Source Type", xlab="Year",
+      ylab="Total Emissions")
 
 ## close graphics device
 dev.off()
